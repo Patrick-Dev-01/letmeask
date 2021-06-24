538930940f5cb1e717f4fda7f6c461269460677b
Patrick <patrick.rdecarvalho@outlook.com>
2021-06-24 14:39:15 -0300

    quarto dia: sala do admin, hook useRoom e funcionalidade dos likes

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,10 +3,12 @@ import { ButtonHTMLAttributes } from "react"
 import '../styles/button.scss';
 
 // todas as proprieades que um botão pode receber
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+    isOutlined?: boolean;
+};
 
-export default function Button(props: ButtonProps){
+export default function Button({ isOutlined = false, ...props }: ButtonProps){
     return (
-        <button className="button" {...props}></button>
+        <button className={`button ${isOutlined ? 'outlined' : ''}`} {...props}></button>
     )
 }
